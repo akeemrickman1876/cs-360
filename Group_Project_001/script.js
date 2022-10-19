5d1621c6103b00d549c40cfcb2b27f347d976f30
SuperArtisMickens <60350581+SuperArtisMickens@users.noreply.github.com>
2022-10-18 21:02:01 -0400

    Update script.js
    worked on in class (KAM) Oct 18, 2022 - 2101

--- Group_Project_001/script.js
+++ Group_Project_001/script.js
@@ -1,1 +1,123 @@
 
+
+/*
+let height = 0;
+let BMI = 0;
+let weight = 0;
+
+
+function getHeight () {
+console.log("Enter your height: ");
+}
+
+function setHeight(newHeight){
+height  =  newHeight;
+}
+
+function getWeight () {
+console.log("Enter your weight: ");
+}
+
+function setWeight(newWeight){
+weight  =  newWeight;
+}
+
+BMI = (weight * height * height);
+
+console.log (BMI)
+
+function calculateMetricBMI(weight, height)
+{
+  let BMI = (weight * height * height);
+  return BMI;
+  
+}
+
+function calculateUSUNITBMI(weight, height)
+{
+
+  let BMI = (weight) ÷ ((height * height) * 703);
+  let BMI = (weight * height * height);
+  return BMI;
+  
+}
+
+
+function calculateBodyFatPercentage(BMI, Age)
+{
+  
+  let BFA = (1.20 x BMI) + (0.23 x Age) - 5.4;
+  return BFA;
+  
+}
+
+// think about whether we give user option to 
+generate BMI or NOT.
+
+// remember that this generates BFA for WOMEN.
+
+
+= Body Fat Percentage
+
+
+
+
+
+function giveBMIRATING(BMI)
+{
+
+  if (BMI < 0) 
+  {
+    return console.log("ERROR");
+  }
+
+  if (BMI <= 18.4) 
+  {
+    return console.log("UNDERWEIGHT ");
+    // think about generating function that gives
+    // advice
+  }
+
+  if ((BMI > 18.4) && (BMI < 25.0))
+  {
+  return console.log("congradulations!  You are ok!  ");
+    // think about generating function that gives
+    // advice
+    }
+
+    if ((BMI > 24.9) && (BMI < 40.0))
+  {
+  return console.log("overweight");
+    // think about generating function that gives
+    // advice
+    }
+
+    if (BMI >= 40.0)
+  {
+  return console.log("oBESE");
+    // think about generating function that gives
+    // advice
+    }
+
+    
+}
+
+
+≤ 18.4	Underweight
+18.5 - 24.9	Normal
+25.0 - 39.9	Overweight
+≥ 40.0	Obese
+
+
+988650
+BMI
+988650
+
+456
+72
+BMI
+988650
+BMI = (weight * height * height);
+2363904
+
+*/
